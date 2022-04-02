0dc0cad2e0a5f4e08bd00b88c3a49cb3f3ecfb3b
Natalia Sharshakova <sharshanatty@gmail.com>
2022-04-02 10:08:58 +0300

    Типизировала функции

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,21 +1,10 @@
-/*
-Типизируйте функцию getPersons, возвращающую массив объектов со следующими свойствами:
-- name - строка
-- age - число
-- gender - 'male' или 'female'
- */
-export function getPersons();
+type User = {
+    name: string;
+    age: number;
+    gender: 'male' | 'female';
+};
+export function getPersons(): User[];
 
-/*
-Типизируйте функцию personToString, возвращающую строку и принимающую объект одного из следующих форматов:
-   1. Пользователь:
-   - name - строка
-   - age - число
-   - gender - 'male' или 'female'
-   2. Сотрудник
-   - name - строка
-   - age - число
-   - gender - 'male' или 'female'
-   - company- строка
- */
-export function personToString(person);
+type Employee = User & { company: string };
+type Person = User | Employee;
+export function personToString(person: Person): string;
